120b67424a6715f62152c35f21dc0d6b1a3eaca6
•_• <abhinavaditya035@gmail.com>
2025-07-13 06:14:31 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,22 +1,21 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
-
 from os import path, getenv
 
 class Config:
     API_ID = int(getenv("API_ID", ""))
     API_HASH = getenv("API_HASH", "")
     BOT_TOKEN = getenv("BOT_TOKEN", "")
-    # Your Force Subscribe Channel Id Below 
+    PORT = os.environ.get("PORT", "8080")
+    
+   # Your other confgs # Your Force Subscribe Channel Id Below 
     CHID = int(getenv("CHID", "")) # Make Bot Admin In This Channel
+    BOT_UPTIME  = time.time()
+    START_PIC   = os.environ.get("START_PIC", "https://ibb.co/DH3N4Lyr")
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    FSUB_PIC = os.environ.get("FSUB_PIC", "")
+    
+    
     # Admin Or Owner Id Below
     SUDO = list(map(int, getenv("SUDO", "").split()))
     MONGO_URI = getenv("MONGO_URI", "")
     
 cfg = Config()
-
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
