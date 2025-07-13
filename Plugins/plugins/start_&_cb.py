51c95a2e313d8e6da10a959b6d65fff1d56ee45c
•_• <abhinavaditya035@gmail.com>
2025-07-13 07:20:07 +0530

    Update start_&_cb.py

--- Plugins/plugins/start_&_cb.py
+++ Plugins/plugins/start_&_cb.py
@@ -82,5 +82,43 @@ async def start(client, message: Message):
         
         return
       
-@Client.on_callback_query()
-async def cb_handler(client, query: CallbackQuery):
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+
+    if data == "help":
+        await query.message.edit_text(
+            text=HELP_TXT.format(first=query.from_user.first_name),
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup([
+                [InlineKeyboardButton('ʜᴏᴍᴇ', callback_data='start'),
+                 InlineKeyboardButton("ᴄʟᴏꜱᴇ", callback_data='close')]
+            ])
+        )
+
+    elif data == "about":
+        await query.message.edit_text(
+            text=ABOUT_TXT.format(first=query.from_user.first_name),
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup([
+                [InlineKeyboardButton('ʜᴏᴍᴇ', callback_data='start'),
+                 InlineKeyboardButton('ᴄʟᴏꜱᴇ', callback_data='close')]
+            ])
+        )
+
+    elif data == "start":
+        await query.message.edit_text(
+            text=START_MSG.format(first=query.from_user.first_name),
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup([
+                [InlineKeyboardButton("ʜᴇʟᴘ", callback_data='help'),
+                 InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data='about')]
+            ])
+        )
+
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
