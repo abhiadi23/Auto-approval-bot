83090dbb90d16db3b1b04842c4b7c6e25be6d944
•_• <abhinavaditya035@gmail.com>
2025-07-13 06:28:08 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,16 +6,21 @@ class Config:
     BOT_TOKEN = getenv("BOT_TOKEN", "")
     PORT = os.environ.get("PORT", "8080")
     
-   # Your other confgs # Your Force Subscribe Channel Id Below 
-    CHID = int(getenv("CHID", "")) # Make Bot Admin In This Channel
+   # Your other confgs 
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "https://ibb.co/DH3N4Lyr")
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
     FSUB_PIC = os.environ.get("FSUB_PIC", "")
     
     
     # Admin Or Owner Id Below
     SUDO = list(map(int, getenv("SUDO", "").split()))
     MONGO_URI = getenv("MONGO_URI", "")
-    
-cfg = Config()
+
+#--------------------------------------------
+START_MSG = os.environ.get("START_MESSAGE", "<b>Bᴀᴋᴀᴀᴀ!!!....{first}\n\n<blockquote>ɪ ᴀᴍ ᴀᴜᴛᴏ ᴀᴘᴘʀᴏᴠᴀʟ ʙᴏᴛ ᴡɪᴛʜ ᴀᴅᴠᴀɴᴄᴇ ғᴇᴀᴛᴜʀᴇs ʏᴏᴜ ᴄᴀɴ ᴀᴅᴅ ᴍᴇ ɪɴ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ᴏʀ ɢʀᴏᴜᴘ ᴀɴᴅ ɪ ᴡɪʟʟ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴀᴘᴘʀᴏᴠᴇ ᴛʜᴇɪʀ ʀᴇǫᴜᴇsᴛs...!!.</blockquote></b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "ʜᴇʟʟᴏ {first}\n\n<b>ᴊᴏɪɴ ᴏᴜʀ ᴄʜᴀɴɴᴇʟs ᴀɴᴅ ᴛʜᴇɴ ᴄʟɪᴄᴋ ᴏɴ ʀᴇʟᴏᴀᴅ button ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ʀᴇǫᴜᴇꜱᴛᴇᴅ ꜰɪʟᴇ.</b>")
+HELP_TXT = "<b><blockquote>Iᴀᴍ ᴀɴ Aᴜᴛᴏ Aᴘᴘʀᴏᴠᴀʟ ʙᴏᴛ ᴀɴᴅ ɴᴏᴛʜɪɴɢ ᴀɴʏ ᴍᴏʀᴇ ᴀɴᴅ ɪ ᴏɴʟʏ ᴡᴏʀᴋ ғᴏʀ ᴍʏ sᴇɴᴘᴀɪ</blockquote></b> \n\n<b><blockquote>Iғ ʏᴏᴜ ᴡᴀɴᴛ ᴏᴜʀ ʜᴇʟᴘ ᴄᴏɴᴛᴀᴄᴛ ᴀᴛ <a href=https://t.me/botskingdoms>Seishiro</a></blockquote></b>"
+ABOUT_TXT = "<b><blockquote>◈ ᴄʀᴇᴀᴛᴏʀ: <a href=https://t.me/seishiro_atanime>Seishiro</a>\n◈ ꜰᴏᴜɴᴅᴇʀ ᴏꜰ : <a href=https://t.me/seishiro_atanime>Seishiro at anime</a>\n◈ Bᴏᴛ ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/+MIFq6K4VQ9tjNThl>Seishiro Bots</a>\n◈ Eɴɢ sᴜʙ/ᴅᴜʙ : <a href=https://t.me/seishiro_atanime>Seishiro at anime</a>\n◈ Hɪɴᴅɪ ᴅᴜʙ : <a href=https://t.me/seishiro_anime_is>Seishiro anime</a></blockquote></b>"
+#--------------------------------------------
